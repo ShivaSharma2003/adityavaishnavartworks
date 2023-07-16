1f555f8c353ce32a4caf808c673f76f4b5ef58d6
Shiva Sharma <shivasharmajpr2003@gmail.com>
2023-07-16 18:16:45 +0530

    Typerwriter effect implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import BackgroundVideo from "../src/Assets/Untitled 31_1080p.mp4";
 import Navbar from "./Components/Navbar";
+import TypewriterComponent from "typewriter-effect";
 
 function App() {
   return (
@@ -10,8 +11,37 @@ function App() {
           <source src={BackgroundVideo} />
         </video>
       </div>
-      <div className="flex flex-col h-full w-[90vw] bg-black opacity-75 px-6 py-4 ">
+      <div className="flex flex-col h-full w-[90vw] bg-black opacity-75 px-6 py-4 overflow-y-scroll scrollbar-hide">
         <Navbar />
+        <div className="flex flex-row w-full justify-between text-white my-6 py-4">
+          <div className="flex-[0.8] flex flex-col justify-center items-center">
+            <h1 className="text-3xl font-bold ">
+              <TypewriterComponent
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString("Hi This is Aditya Vaishanv.")
+                    .pauseFor(100)
+                    .deleteAll()
+                    .typeString("Welcome to AadityaVaishnavartworks")
+                    .start();
+                }}
+              />
+            </h1>
+            <h4 className="font-semibold ">
+              <TypewriterComponent
+                onInit={(typewriter) => {
+                  typewriter
+                    .pauseFor(12000)
+                    .typeString(
+                      "lorem hello world this is aditya waishnav nice to meet find out my new Sketches and exibithions et. etc."
+                    )
+                    .start();
+                }}
+              />
+            </h4>
+          </div>
+          <div>Right Side of the about</div>
+        </div>
       </div>
     </div>
   );
